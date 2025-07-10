8352a3c543a59dac07718a22e593848822566f33
kyriakid1s <kwstasnere@gmail.com>
2025-07-10 21:38:40 +0300

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/portofolio',
-  assetPrefix: '/portofolio',
+
 };
 
 export default nextConfig;
