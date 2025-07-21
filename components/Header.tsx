6e935b83114b7efd3cbe345d5fe6162e7b1e4736
Dimitris Kyriakidis <kwstasnere@gmail.com>
2025-07-21 17:49:47 +0300

    Navbar add bold font in current page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,25 @@
 'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import { FiMenu } from "react-icons/fi";
 
 export default function Header() {
+    const pathname = usePathname()
     const [open, setOpen] = useState(false)
     const initOpen = () => { setOpen(false) }
     return (
-        <header className="bg-code-bg border-b border-terminal-green/20 relative">
+        <header className=" border-b border-white relative">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 <Link href="/" onClick={initOpen} className=" text-xl font-bold">
                     $ D. Kyriakidis
                 </Link>
                 <FiMenu className="md:hidden" onClick={() => setOpen((open) => !open)} />
-                <nav className={`md:flex space-x-6 ${open ? "absolute top-full left-0 w-full bg-black border-1 rounded-xl border-white flex flex-col space-y-2 px-4 py-4 z-50 items-center justify-content-center" : 'hidden'}`} >
-                    <Link href="/about" onClick={initOpen} className="hover:text-terminal-green transition text-left">about</Link>
-                    <Link href="/projects" onClick={initOpen}>projects</Link>
-                    <Link href="/contact" onClick={initOpen} >contact</Link>
-                    <Link href="/blog" onClick={initOpen}>blog</Link>
+                <nav className={`md:flex space-x-6 ${open ? "absolute top-full left-0 w-full bg-black border-1 rounded-xl border-white flex flex-col space-y-2 px-4 py-4 z-10" : 'hidden'}`} >
+                    <Link href="/about" onClick={initOpen} className={`hover:font-bold ${pathname === '/about' ? "font-bold" : "font-normal"}`}>about</Link>
+                    <Link href="/projects" onClick={initOpen} className={`hover:font-bold ${pathname === '/projects' ? "font-bold" : "font-normal"}`}>projects</Link>
+                    <Link href="/contact" onClick={initOpen} className={`hover:font-bold ${pathname === '/contact' ? "font-bold" : "font-normal"}`}>contact</Link>
+                    <Link href="/blog" onClick={initOpen} className={`hover:font-bold ${pathname === '/blog' ? "font-bold" : "font-normal"}`}>blog</Link>
                 </nav>
             </div>
         </header >
