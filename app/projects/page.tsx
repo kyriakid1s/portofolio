fcada15471e8d6aa307cc4471b9f3a5a2a9a6451
Dimitris Kyriakidis <kwstasnere@gmail.com>
2025-07-27 19:44:08 +0300

    Update chat api github link

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,45 +4,45 @@ const projects = [
     {
         title: "Instagram clone",
         description: (<>
-        <p>A fully functional Instagram clone built with Express.js (Node), Vue.js, and MongoDB, featuring:</p>
-        <ul>
-            <li>-User Auth: Secure login/register with JWT & password hashing</li>
-            <li>-Posts: Image uploads</li>
-            <li>-Interactions: Like/comment on posts, follow/unfollow users</li>
-            <li>-Real-time Chat WebSocket-powered direct messaging</li>
-            <li>-Responsive UI Mobile-friendly design</li>
-        </ul>
-    </>),
+            <p>A fully functional Instagram clone built with Express.js (Node), Vue.js, and MongoDB, featuring:</p>
+            <ul>
+                <li>-User Auth: Secure login/register with JWT & password hashing</li>
+                <li>-Posts: Image uploads</li>
+                <li>-Interactions: Like/comment on posts, follow/unfollow users</li>
+                <li>-Real-time Chat WebSocket-powered direct messaging</li>
+                <li>-Responsive UI Mobile-friendly design</li>
+            </ul>
+        </>),
         technologies: ["Node.js", "MongoDB", "Express", "Vue.js", "Websockets"],
         githubUrl: "https://github.com/kyriakid1s/kortsigram",
     },
     {
         title: "File Sharing",
-        description: 
-        (<>
-        <p>A fully functional file sharing app built with Go and Vue.js, featuring:</p>
-        <ul>
-            <li>- End-to-end encryption: Files are encrypted before upload using AES-GCM, ensuring only authorized users can access them.</li>
-            <li>- AWS S3 storage: Scalable and reliable cloud storage for file hosting.</li>
-            <li>- Secure file links: Shareable URLs with optional expiration times for enhanced security.</li>
-        </ul>
-    </>),
+        description:
+            (<>
+                <p>A fully functional file sharing app built with Go and Vue.js, featuring:</p>
+                <ul>
+                    <li>- End-to-end encryption: Files are encrypted before upload using AES-GCM, ensuring only authorized users can access them.</li>
+                    <li>- AWS S3 storage: Scalable and reliable cloud storage for file hosting.</li>
+                    <li>- Secure file links: Shareable URLs with optional expiration times for enhanced security.</li>
+                </ul>
+            </>),
         technologies: ["Go", "AWS S3", "Vue.js"],
         githubUrl: "https://github.com/kyriakid1s/files",
     },
     {
         title: "Real-time chat application API",
-        description: 
-        (<>
-        <p>A fast, secure, and scalable real-time chat api built with Go, designed for both group and private messaging with modern web architecture, featuring:</p>
-        <ul>
-            <li>- Real-Time Messaging: Instant communication powered by WebSockets, built on Go’s high-performance concurrency model.</li>
-            <li>- Secure Authentication: Users authenticate using JWT tokens, enabling protected routes and socket connections.</li>
-            <li>- PostgreSQL Database: Stores user accounts, chat messages, and room metadata with relational integrity and performance.</li>
-        </ul>
-    </>),
+        description:
+            (<>
+                <p>A fast, secure, and scalable real-time chat api built with Go, designed for both group and private messaging with modern web architecture, featuring:</p>
+                <ul>
+                    <li>- Real-Time Messaging: Instant communication powered by WebSockets, built on Go’s high-performance concurrency model.</li>
+                    <li>- Secure Authentication: Users authenticate using JWT tokens, enabling protected routes and socket connections.</li>
+                    <li>- PostgreSQL Database: Stores user accounts, chat messages, and room metadata with relational integrity and performance.</li>
+                </ul>
+            </>),
         technologies: ["Go", "WebSockets", "Concurrent", "Postgres"],
-        githubUrl: "https://github.com/kyriakid1s/chat",
+        githubUrl: "https://github.com/kyriakid1s/chat-api",
     }
 ]
 
